f41818ab5f44f02651e6eb92b7d5c4c1563e0377
Kayden Block <kaydenblock09@gmail.com>
2026-09-25 11:39:29 -0400

    Remove disabledPeriodic method from Robot class
    Removed unused disabledPeriodic method and related code.

--- Robot.java
+++ Robot.java
@@ -7,8 +7,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -206,10 +204,5 @@ public class Robot extends TimedRobot {
         driveSubsystem.stop();
         shooterSubsystem.stop();
         SmartDashboard.putString("Robot Status", "DISABLED");
-    }
-
-    @Override
-    public void disabledPeriodic() {
-        SmartDashboard.putString("Auto Status", "READY - V2");
     }
 }
